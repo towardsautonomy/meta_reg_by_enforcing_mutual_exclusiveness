1660cc1d4006645c81350c8a8dacb9995a6ac493
towardsautonomy <shub.shriv91@gmail.com>
2020-11-01 19:22:34 -0800

    add pose dataset plot

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 ## Omniglot dataset
-# 20-way, 1-shot
-# python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=20 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1
-# python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=20 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1 --mutual_exclusive
+20-way, 1-shot
+python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=20 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1
+python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=20 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1 --mutual_exclusive
 
-# # 10-way, 1-shot
-# python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=10 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1
-# python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=10 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1 --mutual_exclusive
+# 10-way, 1-shot
+python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=10 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1
+python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=10 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1 --mutual_exclusive
 
-# # 5-way, 1-shot
-# python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=5 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1
-# python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=5 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1 --mutual_exclusive
+# 5-way, 1-shot
+python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=5 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1
+python train.py --dataset=omniglot --data_path=datasets/omniglot_resized --n_way=5 --k_shot=1 --meta_batch_size=16 --meta_train_iterations=10000 --num_inner_updates=1 --mutual_exclusive
 
 ## Pose dataset
 # 5-way, 1-shot
